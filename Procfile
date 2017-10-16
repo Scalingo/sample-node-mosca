7aab24d160a9ae969b8e5c10191beb07f4b542f8
Jonathan Hurter <john@scalingo.com>
2017-10-16 17:16:18 +0200

    Fist commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: npm start
+tcp: npm run start-tcp
---
+++ server-tcp.js
@@ -0,0 +1,31 @@
+var mosca = require('mosca');
+
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',
+  url: process.env.MONGO_URL || 'mongodb://localhost:27017/mqtt',
+  pubsubCollection: 'ascoltatori',
+  mongo: {}
+};
+
+var settings = {
+  port: process.env.PORT ? parseInt(process.env.PORT) : 1883,
+  backend: ascoltatore
+};
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
+
+function setup() {
+  console.log('Mosca server is up and running');
+}
